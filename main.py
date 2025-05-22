c49c93ea37458b79d3bb26dc87597aabf45ba625
Raistaid <naranochirov2906@gmail.com>
2025-05-22 18:04:02 +0300

    update

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from database import initialize_database
 import config
 
 if __name__ == '__main__':
-    app.run(port=5003, host='127.0.0.1', debug=True)
+    app.run(port=5003, host='0.0.0.0', debug=True)
